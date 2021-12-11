4fc2b469f86be184fbc011681cfb0733003a72ca
bsamu <bsamuel0220@gmail.com>
2021-12-11 20:38:07 +0100

    practice done

--- script.js
+++ script.js
@@ -1,5 +1,43 @@
+const title = (titleOfForm) => {
+    return `
+        <h1>
+            ${titleOfForm}
+        </h1>
+    `
+}
+
+const inputElement = (type, id, placeholder) => {
+    return `
+        <div class="${id}">
+            <input type="${type}" id="${id}" name="${id}" placeholder="${placeholder}">
+        </div>
+    `
+}
+
+const inputElementCb = (type, id, label, span) => {
+    return `
+        <div class="${id}">
+            <input type="${type}" id="${id}" name="${id}">
+            <label for="${id}">${label}<a href="#">${span}</a></label>
+        </div>
+    `
+}
+
+
+
+const signUpForm = `
+    <form id="form">
+        ${title("Jelentkezés")}
+        ${inputElement("text", "name", "Név")}
+        ${inputElement("email", "email", "Email cím")}
+        ${inputElementCb("checkbox", "terms", "Elolvastam és elfogadom az ", "adatkezelési tájékoztatót.")}
+        <button>Elküld</button>
+    </form>
+`
+
 function loadEvent() {
-    console.log('the page has loaded');
+    const root = document.querySelector("#root");
+    root.insertAdjacentHTML("beforeend", signUpForm)
 }
 
 window.addEventListener("load", loadEvent);
